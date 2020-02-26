dd1316b80ca04ef10550b4825ef8f11c2f89323e
arthur-bryan <arthurbryan2030@gmail.com>
2020-02-26 17:35:32 -0300

    Arquivo adicionado

---
+++ uninstall.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+from time import sleep
+
+CURRENT_DIR = os.getcwd()
+
+if os.getuid() != 0:
+    sys.stdout.write(" [ ! ] Necessário permissões de root (ou sudo)!\n")
+    sys.exit(1)
+else:
+    resposta = input(" [ ? ] Deseja desinstalar e remover por completo o programa carro-de-compras?(S/N):  ").upper()
+    if resposta == "S":
+        sys.stdout.write(" [ - ] Removendo arquivos...\n")
+        with open('files/paths', 'r') as file:
+            for path in file.readlines():
+                os.system('rm -r {}'.format(path))
+            file.close()
+        os.system('rm -r {}'.format(CURRENT_DIR))
+        sleep(2)
+    else:
+        sys.exit(1)
+    sys.stdout.write('[ + ] O programa foi removido com sucesso!')
